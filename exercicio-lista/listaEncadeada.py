257b3d647c368b401abb1a7c638d7935e1810ef0
ANDERSON ROSA DE ALMEIDA <630701670@fspoa.br>
2019-06-05 21:08:38 -0300

    improve list remotion

--- exercicio-lista/listaEncadeada.py
+++ exercicio-lista/listaEncadeada.py
@@ -84,56 +84,79 @@ class ListaEncadeada:
     def printLista(self):
         if self.head is None:
             print("Lista vazia")
+            return False
         
         iter = self.head
-
-        print(iter.value)
+        prev = None        
 
         while iter.next is not None:
-            print(iter.next.value)
+            if prev is None:
+                print('head: ' + str(iter.value))
+            else:
+                print(iter.value)
+
             iter = iter.next
+            prev = iter
+        
+        if iter.next is None:
+            print('tail: ' + str(iter.value))
+
 
     def remove(self, v):
-        if self.head is None:
+        if self.head is None: #se não existe o head a lista não existe, caso 0 elementos
+            print("Lista inexistente")
             return None
         
-        iter = self.head
+        iter = self.head #seta o head como início
 
-        if iter.value == v:
-            if iter.next is None:
+        if iter.value == v: #se o primeiro nó é o valor desejado
+            if iter.next is None: # se não tem um próximo valor, caso 1 elemento
+                self.head = None 
                 print("Lista destruida")
                 return True
-            self.head = iter.next
+            self.head = iter.next #seta como início o próximo nó, caso remove o primeiro elemento
             return True
 
-        prevNo = None
+        prevNo = None #seta o ponteiro que salva o nó anterior
 
-        while iter.next is not None:
+        while iter.next is not None: #itera pela lista, caso N elementos
 
-            if iter.value == v:
-                if prevNo is not None:
-                    prevNo.next = iter.next
+            if iter.value == v: #se é o valor que quero remover                
+
+                if prevNo is None: #verifica se é o head
+                    self.head = iter.next #seta o próximo nó como head
                     return True
-                else:
-                    self.head = iter.next
+                else: #se não é o head
+                    prevNo.next = iter.next #aponta o nó anterior para o próximo nó
                     return True
             
-            prevNo = iter
-            iter = iter.next 
+            prevNo = iter #atualiza a referência para o nó anterior
+            iter = iter.next #atualiza o iterador
+        
+        if iter.value == v:
+            if iter.next is None: #se for o último nó
+                prevNo.next = None #remove o ponteiro do nó anterior
+                self.tail = prevNo # seta o nó anterior como tail
+                return True
+        
+        print('Valor não encontrado na lista.')
 
 
 
 lista = ListaEncadeada()
 
-lista.append(1)
-lista.append(2)
 lista.append(3)
 lista.append(4)
-lista.prepend(5)
-lista.prepend(6)
+lista.append(5)
+lista.append(6)
+lista.prepend(2)
+lista.prepend(1)
 lista.append(7)
 
-# lista.remove(2)
+lista.printLista()
+
+
+lista.remove(5)
 
 
 lista.printLista()
